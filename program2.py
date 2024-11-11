3ec85b013b5a9ce9f3dc95d403b3566caa8fba9a
supriyakatha <kathasupriya@gmail.com>
2024-11-11 15:48:14 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,21 @@
 class Solution(object):
     def romanToInt(self, s):
+        roman_map ={'I':1, 'V':5,'X':10 ,'L':50 ,'C':100 ,'D':500,'M':1000}
+        sum=0
+        i=0
+        while i<len(S):
+            if i+1<len(S) and roman_map[s[i]]<roman_maps[s[i+1]]:
+                sum+=
+               roman_map[s[i+1]]-roman_map[s[i]]
+                i+=2
+            else:
+                sum+=
+                romam_map[s[i]]
+                i+=1
+        return sum
+         if name == "main"
+             s=print(romanToInt(self,s))
+                    
         """
         :type s: str
         :rtype: int
